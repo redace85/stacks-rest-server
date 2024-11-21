89b67017aae174964f88e321910ab88e1e31e603
Jae Liu <jae.liu@matrixport.com>
2024-11-21 17:33:53 +0700

    chore: default port 9999

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express ();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9999;
 
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
